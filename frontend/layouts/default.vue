0de36352aed7aa7356199c9f14c2ebccb66cfa51
Ywoosang <opellong13@gmail.com>
2024-10-03 21:33:33 +0900

    feat: 기본 레이아웃 추가

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <q-layout view="hHh lpR fFf">
+  
+      <q-header elevated class="bg-primary text-white">
+        <q-toolbar>
+          <q-toolbar-title>
+            <q-avatar>
+              <img src="#">
+            </q-avatar>
+            로고 넣을 것
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+  
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+  
+    </q-layout>
+  </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
