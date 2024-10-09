8f933596fe380437ae6f44325e91d9c484d88f97
Ywoosang <opellong13@gmail.com>
2024-10-09 22:19:46 +0900

    feat: 헤더 색상 및 로고 배치 수정

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,28 +1,46 @@
 <template>
-    <q-layout view="hHh lpR fFf">
-  
-      <q-header elevated class="bg-primary text-white">
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="#">
-            </q-avatar>
-            로고 넣을 것
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-  
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-  
-    </q-layout>
-  </template>
+  <q-layout view="hHh lpR fFf">
+    <q-header
+      elevated
+      class="bg-white text-dark row items-center justify-center custom-header"
+    >
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="#" />
+          </q-avatar>
+          로고 넣을 것
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
+.custom-header {
+  height: 70px;
+}
 
+@media (max-width: 600px) {
+  .custom-header {
+    height: 50px;
+  }
+}
+
+@media (min-width: 601px) and (max-width: 1199px) {
+  .custom-header {
+    height: 60px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .custom-header {
+    height: 80px;
+  }
+}
 </style>
