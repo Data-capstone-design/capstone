40c21c80be7ee0ce1b184e3d473acec6460ab39d
Ywoosang <opellong13@gmail.com>
2024-11-17 00:51:01 +0900

    feat: 비디오 높이에 따른 목차 높이 조정

--- frontend/stores/videoStore.ts
+++ frontend/stores/videoStore.ts
@@ -2,6 +2,7 @@ import {type Ref, ref} from "vue";
 
 export interface VideoStore {
     videoPlayer: Ref<any>;
+    videoHeight: Ref<number>;
     videoId: Ref<string>;
     setPlayer: (player: any) => void;
     getPlayer: () => any;
@@ -9,13 +10,16 @@ export interface VideoStore {
     getCurrentVideoTime: () => number;
     setPlayerSize: (windowWidth: number) => void;
     setVideoId: (videoId: string) => void;
+    getPlayerHeight: () => number;
+    seekToTime: (seconds: number) => void
 }
 
 export const useVideoStore = defineStore('video', (): VideoStore => {
     const videoPlayer = ref<any> (null);
     const videoId = ref<string>('')
+    const videoHeight = ref<number> (0)
 
-    const setPlayer = (player: any) => {
+    const setPlayer = (player: any): void => {
         videoPlayer.value = player;
     }
 
@@ -31,21 +35,33 @@ export const useVideoStore = defineStore('video', (): VideoStore => {
         return videoPlayer.value.getCurrentTime();
     }
 
-    const setPlayerSize = (windowWidth: number) => {
+    const setPlayerSize = (windowWidth: number): void => {
         const videoRatio: number = windowWidth > 768 ? 0.5 : 1;
         const width = windowWidth * videoRatio;
         const height = width * (9 / 16);
+        videoHeight.value = height;
         videoPlayer.value.setSize(width, height);
     }
+
+    const seekToTime = (seconds: number): void => {
+        if (videoPlayer && typeof videoPlayer.value.seekTo === "function") {
+            videoPlayer.value.seekTo(seconds, true); // 두 번째 인자는 autoplay 여부 (true: 재생)
+        }
+    };
+
+    const getPlayerHeight = () => videoHeight.value;
 
     return {
         videoPlayer,
         videoId,
+        videoHeight,
         getVideoId,
         setPlayer,
         setVideoId,
         getPlayer,
         getCurrentVideoTime,
-        setPlayerSize
+        setPlayerSize,
+        getPlayerHeight,
+        seekToTime
     }
 })
