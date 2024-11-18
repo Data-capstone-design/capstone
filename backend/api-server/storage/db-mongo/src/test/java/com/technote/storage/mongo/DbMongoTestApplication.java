abc57f17dea07ab269d2db22532b5d7e00cf484a
Ywoosang <opellong13@gmail.com>
2024-11-19 07:43:44 +0900

    chore: db-mongo 테스트 설정

---
+++ backend/api-server/storage/db-mongo/src/test/java/com/technote/storage/mongo/DbMongoTestApplication.java
@@ -0,0 +1,11 @@
+package com.technote.storage.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMongoTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbMongoTestApplication.class, args);
+    }
+}
