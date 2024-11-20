e36da39fcf98776d2ad302d204748691de588a43
Ywoosang <opellong13@gmail.com>
2024-11-21 03:00:33 +0900

    [BE/API] 공통에러/응답 처리 기능을 추가한다. (#100)
    * feat: 공통 응답 클래스 추가
    
    * feat: 에러 관련 클래스 추가
    
    * feat: 전역 에러 처리 handler 추가
    - 아직까지는, CustomException 만 잡도록 구현
    
    * style: 주석제거

---
+++ backend/api-server/core/core-api/src/main/java/com/technote/core/support/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.technote.core.support;
+
+import com.technote.core.support.error.CustomException;
+import com.technote.core.support.error.ErrorType;
+import com.technote.core.support.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<?>> handleCoreException(CustomException e) {
+        switch (e.getErrorType()
+                .getLogLevel()) {
+            case ERROR -> log.error("CustomException : {}", e.getMessage(), e);
+            case WARN -> log.warn("CustomException : {}", e.getMessage(), e);
+            default -> log.info("CustomException : {}", e.getMessage(), e);
+        }
+        return new ResponseEntity<>(ApiResponse.error(e.getErrorType(), e.getData()), e.getErrorType()
+                .getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error("Exception : {}", e.getMessage(), e);
+        return new ResponseEntity<>(ApiResponse.error(ErrorType.DEFAULT_ERROR), ErrorType.DEFAULT_ERROR.getStatus());
+    }
+}
---
+++ backend/api-server/core/core-api/src/main/java/com/technote/core/support/error/ErrorMessage.java
@@ -0,0 +1,24 @@
+package com.technote.core.support.error;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorMessage {
+    private final ErrorCode code;
+
+    private final String message;
+
+    private final Object data;
+
+    public ErrorMessage(ErrorType errorType) {
+        this.code = errorType.getCode();
+        this.message = errorType.getMessage();
+        this.data = null;
+    }
+
+    public ErrorMessage(ErrorType errorType, Object data) {
+        this.code = errorType.getCode();
+        this.message = errorType.getMessage();
+        this.data = data;
+    }
+}
