7d8abe9c89118dad6c3b5905a5072493df737d0e
Ywoosang <opellong13@gmail.com>
2024-11-21 02:52:09 +0900

    feat: 공통 응답 클래스 추가

---
+++ backend/api-server/core/core-api/src/main/java/com/technote/core/support/response/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.technote.core.support.response;
+
+import com.technote.core.support.error.ErrorMessage;
+import com.technote.core.support.error.ErrorType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private final ResultType result;
+
+    private final T data;
+
+    private final ErrorMessage errorMessage;
+
+    public static ApiResponse<?> success() {
+        return new ApiResponse<>(ResultType.SUCCESS, null, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(ResultType.SUCCESS, data, null);
+    }
+
+    public static ApiResponse<?> error(ErrorType error) {
+        return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error));
+    }
+
+    public static ApiResponse<?> error(ErrorType error, Object errorData) {
+        return new ApiResponse<>(ResultType.ERROR, null, new ErrorMessage(error, errorData));
+    }
+}
---
+++ backend/api-server/core/core-api/src/main/java/com/technote/core/support/response/ResultType.java
@@ -0,0 +1,5 @@
+package com.technote.core.support.response;
+
+public enum ResultType {
+    SUCCESS, ERROR
+}
