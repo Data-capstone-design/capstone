415f830b5be129c33d9775dcc7bf112c5a30d256
Ywoosang <opellong13@gmail.com>
2024-11-25 17:41:39 +0900

    feat: 무한스크롤 조회 구현

--- frontend/components/main/MainNotes.vue
+++ frontend/components/main/MainNotes.vue
@@ -1,15 +1,24 @@
 <template>
   <section class="q-pa-md q-mt-xl" style="max-width: 70%">
-    <h1 class="ns-Title">준비된 노트들<q-icon name="book" class="q-mb-xs q-pl-xs" style="color:#0e0e0e" /></h1>
-    <div class="row q-col-gutter-md justify-start full-width">
+    <h1 class="ns-Title">
+      <q-icon name="book" class="q-mb-xs q-pl-xs" style="color:#0e0e0e" />
+      <span>전체 노트</span>
+      <span class="text-grey-6 q-pl-sm" style="font-size: .8rem">더 보려면 스크롤 하세요</span>
+    </h1>
+    <div
+        class="row q-col-gutter-md justify-start full-width"
+        @scroll="onScroll"
+        ref="scrollContainer"
+        style="overflow-y: auto; max-height: 500px;"
+    >
       <q-card
-          v-for="(item, index) in youtubeItems"
+          v-for="(item, index) in notePreviewItems"
           :key="index"
           class="fixed-card col-12 col-sm-6 col-md-4 column"
           style="padding: 0; margin: 10px;"
       >
         <img
-            :src="item.thumbnail"
+            :src="`https://img.youtube.com/vi/${item.thumbnail}/default.jpg`"
             class="q-card-image"
             alt="노트 이미지"
         />
@@ -24,14 +33,13 @@
         </q-card-section>
 
         <q-card-actions class="no-padding  row justify-between">
-
           <q-btn
               flat
               color="grey-7"
               target="_blank"
               label="노트 보기"
               icon="book"
-              @click="note"
+              @click="goToNote(item.videoId,item.userLevel)"
           />
         </q-card-actions>
 
@@ -40,154 +48,92 @@
             class="q-ml-xs u-Level"
         ></div>
       </q-card>
-
-
     </div>
+    <div v-if="loading" class="text-center q-my-md text-bold">로딩중 . . .</div>
   </section>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref, onMounted } from "vue";
+import {useNoteStore} from "~/stores/noteStore";
 
+const noteStore = useNoteStore();
 
+interface NotePreview {
+  noteId: string;
+  videoId: string;
+  userLevel: string;
+  title: string;
+  thumbnail: string;
+}
 
-const youtubeItems = ref([
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
+interface PagedNotePreviews {
+  notePreviews: NotePreview[];
+  nextCursor: string;
+  hasMore: boolean;
+}
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
+const notePreviewItems = ref<NotePreview[]>([]);
+const nextCursor = ref<string | null>(null);
+const hasMore = ref(true);
+const loading = ref(false);
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
+const config = useRuntimeConfig();
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
+const fetchNotes = async () => {
+  if (loading.value || !hasMore.value) return;
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
+  loading.value = true;
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
+  try {
+    let params;
+    if(notePreviewItems.value.length > 0) {
+      params = {
+        lastId: nextCursor.value,
+        pageSize: 20,
+      }
+    } else {
+      params = {
+        pageSize: 20
+      }
+    }
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
+    const response = await $fetch<PagedNotePreviews>('/notes/preview', {
+      method: 'GET',
+      baseURL: config.public.apiBaseUrl,
+      params
+    });
 
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "INTERMEDIATE"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "BASIC"
-
-  },
-  {
-    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
-    title: "Never Gonna Give You Up",
-    userLevel: "ADVANCED"
+    notePreviewItems.value.push(...response.data.notePreviews);
+    nextCursor.value = response.data.nextCursor;
+    hasMore.value = response.data.hasMore;
+  } catch (error) {
+    console.error("Failed to fetch notes:", error);
+  } finally {
+    loading.value = false;
   }
+};
 
+const onScroll = (event: Event) => {
+  const target = event.target as HTMLElement;
 
-]);
+  if (target.scrollHeight - target.scrollTop <= target.clientHeight + 100) {
+    fetchNotes();
+  }
+};
+
+const goToNote = async (videoId: string, userLevel: string): Promise<void> => {
+    const response = await noteStore.fetchNoteStatus(videoId,userLevel);
+    const { noteStatus } = response.data;
+    noteStore.setNoteInfo(videoId, userLevel, noteStatus);
+    navigateTo("/note");
+};
+
+onMounted(() => {
+  fetchNotes();
+});
 </script>
+
 
 <style scoped>
 
